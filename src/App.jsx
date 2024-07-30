0e08cca8b76642c381e9f17491f42790c6554d6a
mvula <mvulakolweni08@gmail.com>
2024-07-30 21:13:43 +0200

    initialized files for reacts, and set up the navbar component, and set backgroud to black

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
