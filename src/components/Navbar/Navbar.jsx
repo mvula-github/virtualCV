fb0bf823f6c5a1415e22d503a7145b39fdf3271a
mvula <mvulakolweni08@gmail.com>
2024-07-30 21:34:29 +0200

    horizontally aligned menu items using css, made text bold(500 wght), and distrubuted items equally

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,18 @@ import "./Navbar.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="nav-container">
+        <div className='nav-content'>
+            <h3 className='logo'><a>ePortfolio</a></h3>
+            <ul>
+                <li><a className='menu-item'>home</a> </li>
+                <li><a className='menu-item'>about</a> </li>
+                <li><a className='menu-item'>projects</a> </li>
+                <li><a className='menu-item'>skills</a> </li>
+                <li><a className='menu-item'>contacts</a> </li>
+            </ul>
+        </div>
+    </nav>
   )
 }
 
