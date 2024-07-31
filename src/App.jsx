b23a97d040b202b936f45185d44a55cdee149a13
mvula <mvulakolweni08@gmail.com>
2024-07-31 11:01:46 +0200

    added the home section component of the website, and set max width to 1300px

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 /* eslint-disable no-unused-vars */
 
 import React from "react";
+import "./App.css"
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 const App = () => {
 
   return (
     <>
       <Navbar />
+
+      <div className="container">
+        <Home />
+      </div>
+
     </>
   );
 };
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
