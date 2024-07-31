dde900b9b38089f48d8398ef6baa34b60d2ec77e
mvula <mvulakolweni08@gmail.com>
2024-08-01 01:54:31 +0200

    fixed onClick bug for when selecting which skill i want to display

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import "./Skills.css";
 import { SKILLS } from '../../utilities/data';
 import SkillCard from './SkillCard/SkillCard';
+import SkillsInfoCard from './SkillsInfoCard/SkillsInfoCard';
 
 const Skills = () => {
+
+    const [selectedSkill,setSelectedSkill] = useState(SKILLS[0]);
+
+    const handleSelectedSkill = (data) =>{
+        setSelectedSkill(data);
+    };
+
   return (
     <section className='skills-container'>
         <h5> Technical Proficiency</h5>
@@ -16,11 +24,21 @@ const Skills = () => {
                         key ={item.title}
                         iconUrl={item.icon}
                         title={item.title}
-                     />   
+                        isActive ={selectedSkill.title === item.title}
+                        onClick ={()=> {
+                        handleSelectedSkill(item);
+
+                    }}
+                    />   
                 ))}
             </div>
 
-            <div className='skills-info'></div>
+            <div className='skills-info'>
+                <SkillsInfoCard 
+                    heading={selectedSkill.title}
+                    skills={selectedSkill.skills}
+                />
+            </div>
 
         </div>
     </section>
