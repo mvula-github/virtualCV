f623e41a514f889bd17e31d54152944fb6417469
mvula <mvulakolweni08@gmail.com>
2024-07-31 02:56:24 +0200

    imported mobile navigation compononent to main website component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,40 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, {useState} from 'react'
 import "./Navbar.css"
+import MobileNav from "./MobileNav/MobileNav"
 
 const Navbar = () => {
 
-  
-  return (
-    <nav className="nav-container">
-        <div className='nav-content'>
-            <h3 className='logo'>ePortfolio</h3>
-            <ul>
-                <li><a className='menu-item'>home</a> </li>
-                <li><a className='menu-item'>about</a> </li>
-                <li><a className='menu-item'>projects</a> </li>
-                <li><a className='menu-item'>skills</a> </li>
-                <li><a className='menu-item'>contacts</a> </li>
-            </ul>
+  const [openMenu, setOpenMenu] = useState(false);
 
-            <button className='menu-btn' onClick={()=>{}}>
-                <span className={"material-symbol-outlined"} style={{fontSize:"0.8rem"}}>
-                    menu
-               </span>
-            </button>
-        </div>
-    </nav>
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu)
+
+  };
+
+  return (
+    <>
+      <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
+
+      <nav className="nav-container">
+          <div className='nav-content'>
+              <h3 className='logo'>ePortfolio</h3>
+              <ul>
+                  <li><a className='menu-item'>home</a> </li>
+                  <li><a className='menu-item'>about</a> </li>
+                  <li><a className='menu-item'>projects</a> </li>
+                  <li><a className='menu-item'>skills</a> </li>
+                  <li><a className='menu-item'>contacts</a> </li>
+              </ul>
+
+              <button className='menu-btn' onClick={toggleMenu}>
+                  <span className={"material-symbol-outlined"} style={{fontSize:"0.8rem"}}>
+                      {openMenu ? "close": "menu"}
+                </span>
+              </button>
+          </div>
+      </nav>
+    </>
   )
 }
 
