84a2a612bb06a5c0718e7613a60f3b8c8b11bee4
mvula <mvulakolweni08@gmail.com>
2024-08-01 02:55:19 +0200

    finished percentage animation for when selecting different skill on skill-info-card component

--- src/components/Skills/SkillsInfoCard/SkillsInfoCard.jsx
+++ src/components/Skills/SkillsInfoCard/SkillsInfoCard.jsx
@@ -8,7 +8,7 @@ const SkillsInfoCard = ({heading,skills}) => {
   return (
     <div className='skills-info-card'>
         <h6>{heading}</h6>
-        <div className='skill-info-content'>
+        <div className='skills-info-content'>
             { skills.map((item, index) => (
                 <React.Fragment key={`skill_${index}`}>
                     <div className='skill-info'>
