5b0df24b2c2c70d9f4e67e848b0733143e839a18
mvula <mvulakolweni08@gmail.com>
2024-08-01 15:58:21 +0200

    imported all information in datajs to work_card component

--- src/components/Work/WorkCard/WorkCard.jsx
+++ src/components/Work/WorkCard/WorkCard.jsx
@@ -1,11 +1,22 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import "./WorkCard.css";
 
-// eslint-disable-next-line react/prop-types, no-unused-vars
+
 const WorkCard = ({ details }) => {
   return (
-    <div>WorkCard</div>
+    <div className='work-xp-card'>
+        <h6> {details.title}</h6>
+
+        <div className='work-duration'> {details.date}</div>
+        <ul>
+            {details.responsibilities.map((item) => (
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+
+    </div>
   )
 }
 
