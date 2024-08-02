d7cf155ce822bb69b8c971aa7ade1b7968fca36f
mvula <mvulakolweni08@gmail.com>
2024-08-02 02:09:12 +0200

    added scroll-bar on index.css to try and reduce website shaking, fix resolution for the contact me component/section

--- src/components/ContactMe/ContactForm/ContactForm.jsx
+++ src/components/ContactMe/ContactForm/ContactForm.jsx
@@ -5,14 +5,14 @@ import "./ContactForm.css"
 const ContactForm = () => {
   return (
     <div className='contact-form-content'>
-        <form action="">
+        <form>
             <div className='name-container'>
                 <input type="text" name="firstname" placeholder='First Name'/>
                 <input type="text" name="lastname" placeholder='Last Name'/>
             </div>
 
             <input type="text" name="email" placeholder='Email'/>
-            <textarea type="text" name="message" placeholder="Message" rows={8}/>
+            <textarea type="text" name="message" placeholder="Message" rows={5}/>
 
             <button>Send</button>
         </form>
