0e623f79166bc93184a7fa0ea4a220e5eda17b86
mvula <mvulakolweni08@gmail.com>
2025-01-08 12:19:41 +0200

    updated myabout me text and it's font sizing and also line height

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,27 +1,38 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import "./AboutMe.css"
+import React from "react";
+import "./AboutMe.css";
 
 const AboutMe = () => {
   return (
     <section className="about-container">
-
-    {/* picture block */}
-    <div className="about-img">
+      {/* picture block */}
+      <div className="about-img">
         <div>
-            <img className="Me" src="./assets/Me/me5.jpg" alt="" />
+          <img className="Me" src="./assets/Me/me5.jpg" alt="" />
         </div>
-    </div>
+      </div>
 
-    {/* The heading or my statement block */}
-    <div className='about-content'>
+      {/* The heading or my statement block */}
+      <div className="about-content">
         <h5>About Me</h5>
         <p>
-        As I mention before, I am Mvula Kolweni and I am an aspiring IT specialist, currently pursuing a Bachelor of Science degree in Information Technology at North-West University with an interest in system analysis, design, and development. I am proficient in various programming languages but I highly favour C# and Java. I possess a strong foundation in data structures, algorithms, and database management systems principles and more . I am seeking opportunities to expand my skills and learn from experienced or industry professionals.
+          I’m a recent graduate from North-West University with a Bachelor’s
+          degree in Information Technology, currently pursuing an Honours in
+          Computer Science & Information Technology. I’m highly motivated and
+          passionate about problem-solving, with a strong foundation in IT
+          principles. I’m eager to take on a challenging entry-level role in a
+          dynamic and innovative tech environment where I can contribute to
+          meaningful projects and grow professionally. My skills include
+          object-oriented programming, database management systems, and a
+          variety of technical tools, which I’ve applied successfully in
+          academic projects and as a part-time Student Assistant. I thrive both
+          independently and in team settings, and I’m always ready to adapt,
+          learn new technologies, and bring value to a forward-thinking
+          organization.
         </p>
-    </div>
-   </section>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
