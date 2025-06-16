d912dde0a6189c34cbd7553ec01a625c4669c163
mvula <mvulakolweni08@gmail.com>
2025-06-16 16:47:22 +0200

    changed colors of skillCards

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,47 +1,45 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Skills.css";
-import { SKILLS } from '../../utilities/data';
-import SkillCard from './SkillCard/SkillCard';
-import SkillsInfoCard from './SkillsInfoCard/SkillsInfoCard';
+import { SKILLS } from "../../utilities/data";
+import SkillCard from "./SkillCard/SkillCard";
+import SkillsInfoCard from "./SkillsInfoCard/SkillsInfoCard";
 
 const Skills = () => {
+  const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
 
-    const [selectedSkill,setSelectedSkill] = useState(SKILLS[0]);
-
-    const handleSelectedSkill = (data) =>{
-        setSelectedSkill(data);
-    };
+  const handleSelectedSkill = (data) => {
+    setSelectedSkill(data);
+  };
 
   return (
-    <section className='skills-container'>
-        <h5> Technical Proficiency</h5>
+    <section className="skills-container">
+      <h5> Technical Proficiency</h5>
 
-        <div className='skills-content'>
-            <div className="skills">
-                {SKILLS.map((item) =>(
-                    <SkillCard 
-                        key ={item.title}
-                        iconUrl={item.icon}
-                        title={item.title}
-                        isActive ={selectedSkill.title === item.title}
-                        onClick ={()=> {
-                        handleSelectedSkill(item);
-                    }}
-                    />   
-                ))}
-            </div>
-
-            <div className='skills-info'>
-                <SkillsInfoCard 
-                    heading={selectedSkill.title}
-                    skills={selectedSkill.skills}
-                />
-            </div>
-
+      <div className="skills-content">
+        <div className="skills">
+          {SKILLS.map((item) => (
+            <SkillCard
+              key={item.title}
+              iconUrl={item.icon}
+              title={item.title}
+              isActive={selectedSkill.title === item.title}
+              onClick={() => {
+                handleSelectedSkill(item);
+              }}
+            />
+          ))}
         </div>
+
+        <div className="skills-info">
+          <SkillsInfoCard
+            heading={selectedSkill.title}
+            skills={selectedSkill.skills}
+          />
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Skills;
--- src/components/Skills/SkillsInfoCard/SkillsInfoCard.jsx
+++ src/components/Skills/SkillsInfoCard/SkillsInfoCard.jsx
@@ -1,30 +1,35 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 import "./SkillsInfoCard.css";
 
-
-const SkillsInfoCard = ({heading,skills}) => {
+const SkillsInfoCard = ({ heading, skills }) => {
   return (
-    <div className='skills-info-card'>
-        <h6>{heading}</h6>
-        <div className='skills-info-content'>
-            { skills.map((item, index) => (
-                <React.Fragment key={`skill_${index}`}>
-                    <div className='skill-info'>
-                        <p>{item.skill}</p>
-                        <p className='percentage'>{item.percentage}</p>
-                    </div>
-
-                    <div className='skill-progress-bg'>
-                        <div className='skill-progress' style = {{width: item.percentage}}> </div>
-                    </div>
-                </React.Fragment>
-            ))}
-
-        </div>
+    <div className="skills-info-card">
+      <h2 className="skills-info-heading">{heading}</h2>
+      <div className="skills-info-content">
+        {skills.map((item, index) => (
+          <React.Fragment key={item.skill || `skill_${index}`}>
+            <div className="skill-info">
+              <p>{item.skill}</p>
+              <p className="percentage">{item.percentage}</p>
+            </div>
+            <div className="skill-progress-bg">
+              <div
+                className="skill-progress"
+                style={{ width: item.percentage }}
+                role="progressbar"
+                aria-valuenow={parseInt(item.percentage)}
+                aria-valuemin={0}
+                aria-valuemax={100}
+                aria-label={`${item.skill} proficiency`}
+              />
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SkillsInfoCard;
