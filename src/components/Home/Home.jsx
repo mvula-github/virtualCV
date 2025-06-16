bd6e4498143d3b23a7a9244ceff5f4df7cf2f7df
mvula <mvulakolweni08@gmail.com>
2025-06-16 15:59:54 +0200

    improved semantics and accessible markup, button styled as a link for download, improved color contrast na dfocus/hover states; visual polish for image and layout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,31 +4,39 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <section className="home-container">
+    <section className="home-container" aria-label="Home section">
       {/* The heading or my statement block */}
       <div className="home-content">
-        <h2>
+        <h1>
           Adventures &#128747; in my Amazing World &#x1F30D; <br /> of
           Technology
-        </h2>
+        </h1>
         <p>
-          Hey there &#128075;, I am Mvula a recent IT graduate, currently
+          Hey there &#128075;, I am Mvula, a recent IT graduate, currently
           pursuing my honours degree. I am a ManCity Fan, and I really aspire to
           be an IT Specialist. So this is my e-Portfolio &#128515;.
         </p>
         <a
           href="./assets/docx/Mvula Kolweni CV.pdf"
           download="mvula_kolweni-CV"
-          target="blank"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Download Mvula Kolweni's CV as PDF"
+          className="home-btn-link"
         >
-          <button className="home-btn">Get CV</button>
+          Get CV
         </a>
       </div>
 
       {/* picture block */}
       <div className="home-img">
         <div>
-          <img className="Me" src="./assets/Me/myself.jpg" alt="" />
+          <img
+            className="Me"
+            src="./assets/Me/myself.jpg"
+            alt="Portrait of Mvula Kolweni"
+            loading="lazy"
+          />
         </div>
       </div>
     </section>
